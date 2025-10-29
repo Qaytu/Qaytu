0a497c1d76213d86314610441db4d5119167e797
Qaytu <qaytu.ch@gmail.com>
2025-10-29 18:09:24 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('La página está cargada y lista para la interactividad. (Carrusel animado por CSS)');
+    
+    // Aquí iría cualquier otra funcionalidad que necesites, como un menú hamburguesa
+    // o validaciones de formularios.
+});
